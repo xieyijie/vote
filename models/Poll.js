fe870f45e327b156bb81759b60139b2af42cc165
Richard Xie <xieyijie@gmail.com>
2015-03-24 15:46:54 +1100

    Initial commit

---
+++ models/Poll.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yixie on 24/03/15.
+ */
+var mongoose = require('mongoose');
+
+// Subdocument schema for votes
+var voteSchema = new mongoose.Schema({ ip: 'String' });
+
+// Subdocument schema for poll choices
+var choiceSchema = new mongoose.Schema({
+    text: String,
+    votes: [voteSchema]
+});
+
+// Document schema for polls
+exports.PollSchema = new mongoose.Schema({
+    question: { type: String, required: true },
+    choices: [choiceSchema]
+});
